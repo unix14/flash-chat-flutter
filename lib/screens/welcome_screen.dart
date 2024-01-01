2b646cc781727f647249036799e1dc616d56e7d5
Eyal Yaakobi <unix14@gmail.com>
2024-01-01 23:17:04 +0200

    add animation to app name

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flash_chat/interfaces/identifiable.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
@@ -57,9 +58,9 @@ class _WelcomeScreenState extends State<WelcomeScreen>  with SingleTickerProvide
                     height: 60.0,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                   ),
