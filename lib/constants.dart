dae2e1488890bf72b0a5dbad498119d931fb82e1
Eyal Yaakobi <unix14@gmail.com>
2024-01-06 19:45:29 +0200

    177. Code Refactoring Challenge

--- lib/constants.dart
+++ lib/constants.dart
@@ -17,3 +17,17 @@ const kMessageContainerDecoration = BoxDecoration(
     top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
   ),
 );
+
+const kOutlineBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+);
+
+
+var kInputDecoration = InputDecoration(
+  hintText: '',
+  contentPadding:
+  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: kOutlineBorder,
+  enabledBorder: kOutlineBorder.copyWith(borderSide: BorderSide(color: Colors.blueAccent, width: 1.0)),
+  focusedBorder:kOutlineBorder.copyWith(borderSide: BorderSide(color: Colors.blueAccent, width: 2.0)),
+);
